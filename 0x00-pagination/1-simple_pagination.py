bf72f66af26349cc2d2fdc2a4f879ab303576321
darryl Nnon <darrylnnon@gmail.com>
2024-06-23 11:58:06 +0000

    done

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
-"""Simple pagination"""
+""" Simple pagination """
 import csv
 import math
 from typing import List
+
+
+def index_range(page, page_size):
+    """ return a tuple of size two containing a start index and an end index
+        corresponding to the range of indexes to return in a list for those
+        particular pagination parameters. """
+    if page and page_size:
+        start_index = (page - 1) * page_size
+        end_index = start_index + page_size
+        return start_index, end_index
 
 
 class Server:
@@ -25,18 +35,12 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Return the appropriate page of the dataset"""
-        assert isinstance(page, int) and page > 0
-        assert isinstance(page_size, int) and page_size > 0
-
+        """ obtains the indexes and return corresponding pages """
+        assert type(page) == int and page > 0
+        assert type(page_size) == int and page_size > 0
         start, end = index_range(page, page_size)
-
-        if start >= len(self.dataset()) or end > len(self.dataset()):
-            return []
-
-        return self.dataset()[start:end]
-
-
-def index_range(page, page_size):
-    """Return a tuple of size two containing a start index and an end index"""
-    return ((page - 1) * page_size, page * page_size)
+        pages = []
+        if start >= len(self.dataset()):
+            return pages
+        pages = self.dataset()
+        return pages[start:end]
